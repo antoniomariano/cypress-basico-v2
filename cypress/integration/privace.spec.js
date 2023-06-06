5c4c4cae49c9ba16e9bf2f5e5b880059fa49deaa
Antonio Mariano <antonio@g.globo>
2023-06-06 09:52:23 -0300

    comentando arquivo

--- cypress/integration/privace.spec.js
+++ cypress/integration/privace.spec.js
@@ -1,8 +1,8 @@
 
-    it('Testar a página de política de privacidade de forma independente', function(){
-        cy.visit('./src/privacy.html')
-        cy.contains('Talking About Testing').should('be.visible')
-    })
+    // it('Testar a página de política de privacidade de forma independente', function(){
+    //     cy.visit('./src/privacy.html')
+    //     cy.contains('Talking About Testing').should('be.visible')
+    // })
     // it.only('Testa a página de política de privacidade de forma independente', function(){
     //     //abrindo em outra aba do browser
     //     cy.get('#privacy a')
@@ -11,3 +11,26 @@
             
     //     cy.contains('Talking About Testing').should('be.visible')
     // })
+
+    // Cypress._.times(5, function() {
+    //     it('Testar a página de política de privacidade de forma independente', function(){
+    //         cy.visit('./src/privacy.html')
+    //         cy.contains('Talking About Testing').should('be.visible')
+    //     })
+    // })
+    // it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    //     cy.get('.success')
+    //       .should('not.be.visible')
+    //       .invoke('show')
+    //       .should('be.visible')
+    //       .and('contain', 'Mensagem enviada com sucesso.')
+    //       .invoke('hide')
+    //       .should('not.be.visible')
+    //     cy.get('.error')
+    //       .should('not.be.visible')
+    //       .invoke('show')
+    //       .should('be.visible')
+    //       .and('contain', 'Valide os campos obrigatórios!')
+    //       .invoke('hide')
+    //       .should('not.be.visible')
+    //   }) 
