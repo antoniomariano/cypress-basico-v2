4c128ab7bfb999b0ac9dedb93a0b67b952b5af00
Antonio Mariano <antonio@g.globo>
2023-06-12 12:55:30 -0300

    invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -186,4 +186,24 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('#phone-checkbox').check()
         })
     })
+    //invoke
+    it.only('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      }) 
+
+
+
   })
